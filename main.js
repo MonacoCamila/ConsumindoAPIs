fc5d2ff486f8cf3cac3776f86a4c308f3714bfa3
MonacoCamila <camilamonaco@gmail.com>
2022-09-13 18:44:00 -0300

    projeto completo

---
+++ main.js
@@ -0,0 +1,42 @@
+
+async function buscaEndereco(cep) {
+    var mensagemErro = document.getElementById("erro");
+    mensagemErro.innerHTML = "";
+
+  try {
+    var consultaCep = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+    var consultaCepConvertida = await consultaCep.json();
+    if(consultaCepConvertida.erro){
+        throw Error("cep nao existe");
+    }
+
+    var cidade = document.getElementById("cidade");
+    var logradouro = document.getElementById("endereco");
+    var estado = document.getElementById("estado");
+
+    cidade.value = consultaCepConvertida.localidade;
+    logradouro.value = consultaCepConvertida.logradouro;
+    estado.value = consultaCepConvertida.uf;
+    
+    console.log(consultaCepConvertida);
+    return consultaCepConvertida;
+  } catch (erro) {
+
+    mensagemErro.innerHTML = `<p>CEP invalido. tente novamente!</p>`;
+    
+    console.log(erro)
+  }
+}
+
+var cep = document.getElementById("cep");
+cep.addEventListener('focusout', () => buscaEndereco(cep.value));
+
+
+
+
+
+
+
+
+
+
